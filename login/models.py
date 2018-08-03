f663da065170b5d6799212a456ed9f0dd2e780e4
chetandeokar <chetan.deokar@rmlagtech.com>
2018-08-03 16:55:27 +0530

    User model

--- login/models.py
+++ login/models.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 # Create your models here.
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
+    active = models.BooleanField()
